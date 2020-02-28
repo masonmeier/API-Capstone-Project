f432f281f747d0e9c9bc4d204504e8b7dc09b948
masonmeier <57198981+masonmeier@users.noreply.github.com>
2020-02-27 18:55:48 -0800

    Updated Index.js
    Adjusted for new API input, added render function, cleaned code.

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 'use strict';
 
-//format query//
+let state = {
+  error: '', 
+  adding: false,
+  holidays: []
+}
+
 $(document).ready(function(){
+    state.adding = true;
+    render();
     $('#submit-btn').on('click', function(e){
       e.preventDefault();
       
-      const nation = $('.nation-val').val();
-      const month = $('.month-val').val();
+      const nation = $('#nation-val').val();
+      const month = $('#month-val').val();
       const apiKey = 'fc1ab01148fbada75e56010b3c2533bba3918d06';
       const key = apiKey;
       const monthuri  = 'https://calendarific.com/api/v2/holidays?api_key='+key+'&country='+nation+'&year=2019&month='+month+'';
@@ -20,38 +27,49 @@ $(document).ready(function(){
           $('#hdapidata').html("<h2>Nation not found!</h2>");
         }
 
-        //return JSON object//
-
-        else {(outputHolidays);
-        
-        function outputHolidays() {
-            let outhtml='';
-            let holidays= response.holidays
-            if(holidays.length == 0) { outhtml = outhtml + '<p>0 holidays found!</p></div>'; }
-            else {
-              outhtml = outhtml + '<p><strong>Holidays:</strong></p> <ul>';
-              $.each(holidays, function(index) {
-
-                outhtml += '<a target=”_blank” href=”http://www.google.com/search?q='+holidays[index].name+'”><h2>'+holidays[index].name+' <span class="smallname"></h2></a>';
-                //link and title
-                outhtml += outhtml + '<div class="holiday-content"><div class="country><h3>'+holidays[index].country.name+'</h3></div><div class="date"><h3>'+holidays[index].date.datetime+'</h3></div></div>'
-                
-                outhtml += outhtml + '<li>'+holidays[index]+'</li>';
-              });
-              outhtml = outhtml + '</ul></div>';
-            }
-            $('#hdapidata').html(outhtml);
-          } 
+        else {
+          state.holidays = json.response.holidays;
+          if (!state.holidays.length){
+          state.error = "no holidays found!"
+          }
+          state.adding = false;
+          render();
+          state.error = '';
         } 
       }); 
     }); 
 });
 
+function render() {
+  let outhtml='';
+  let holidays= state.holidays
+  let container = `   
+  <div id="page">
+    <div id="input-page">
+      <h2>Input 1-3 letter nation code</h2><input type="text" id="nation-val"><br>
+      <h2>Input 1-2 character month code</h2><input type="text" id="month-val">
+      <input type="submit" id="submit-btn">
+    </div>
+  </div>`;
+  if (state.adding){
+    outhtml += container
+  };
 
-//questions//
-//Add a restart button to this
-//why isnt this working
-//any breaks in logic
-//enforce limitations on input field
-//Do I need a watchform function or is that handled by line 5?
+  if(state.error) { outhtml = outhtml + `<p>${state.error}</p>`; }
+  if (state.holidays.length){
+    outhtml = outhtml + '<p><strong>Holidays:</strong></p> <ol>';
+  $.each(holidays, function(index) {
+    console.log(index, holidays[index].country.name);
+    outhtml += '<li>'
+    outhtml += '<a target=”_blank” href=”http://www.google.com/search?q='+holidays[index].name+'”><h2>'+holidays[index].name+' <span class="smallname"></h2></a>';
+    outhtml += '<div class="holiday-content"><div class="country><h3>'+holidays[index].country.name+'</h3></div><div class="date"><h3>'+holidays[index].date.iso+'</h3></div></div>';
+    outhtml += '</li>'
+  });
+    outhtml += '</ol></div>';
+  }
+
+
+  $('main').html(outhtml);
+} 
+
 
